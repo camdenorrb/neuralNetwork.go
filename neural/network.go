3c8f880620df56076a4f5e54559cb4a7531be2c3
CoderCowz <140582844+CoderCowz@users.noreply.github.com>
2023-09-12 09:43:02 -0700

    More generic network.go

--- neural/network.go
+++ neural/network.go
@@ -1,8 +1,29 @@
 package neural
 
+import "gonum.org/v1/gonum/mat"
+
 type Network struct {
-	InputNodes   uint64
-	HiddenNodes  uint64
-	OutputNodes  uint64
-	LearningRate mat.Dense
+	headNode NetworkNode // The first node in the network (input layer)
+}
+
+type NetworkNode interface {
+	forward(input mat.Matrix)
+
+	result() mat.Matrix
+
+	next() *NetworkNode
+}
+
+func (n *Network) forward(input mat.Matrix) {
+	n.headNode.forward(input)
+}
+
+func (n *Network) resultOfLastNode() mat.Matrix {
+	node := n.headNode
+	next := node.next()
+	for next != nil {
+		node = *next
+		next = node.next()
+	}
+	return node.result()
 }
